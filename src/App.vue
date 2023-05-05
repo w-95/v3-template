7106efda867030abd86fdf85bbdc0c13d71e3c5d
boxin <w95_16888@163.com>
2023-05-05 11:10:58 +0800

    add: element-plus

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import HelloWorld from './components/HelloWorld.vue';
 
 <template>
   <div>
+    <el-button type="primary">Primary</el-button>
     <p class="test-color">测试sass全局样式</p>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import elementPlus from 'element-plus';
 
 import './style.css';
+import 'element-plus/dist/index.css';
 
 import App from './App.vue';
 
@@ -9,6 +11,7 @@ import router from './router/router';
 
 const app = createApp(App);
 
+app.use(elementPlus);
 app.use(router);
 app.use(createPinia());
 
